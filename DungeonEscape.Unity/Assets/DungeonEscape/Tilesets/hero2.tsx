047c4495166e783f3d62552fe5dcbf4b4057c914
Christiaan Burchell <cjburchell@yahoo.com>
2026-05-06 23:08:37 -0400

    Add new hero sprite and tileset for updated character design
    Integrates a new 'hero-v2' sprite and corresponding tileset 'hero2.tsx' to reflect an updated character design, enhancing visual elements within the game. Includes meta files for Unity integration.

---
+++ DungeonEscape.Unity/Assets/DungeonEscape/Tilesets/hero2.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.1" name="hero2" tilewidth="75" tileheight="100" tilecount="204" columns="16">
+ <grid orientation="orthogonal" width="32" height="48"/>
+ <image source="../Images/sprites/hero-v2.png" width="1200" height="700"/>
+</tileset>

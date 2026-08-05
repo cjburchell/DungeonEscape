114654f97c830e342843df8c7a891dec82fe9d5b
Christiaan Burchell <cjburchell@yahoo.com>
2026-08-05 16:23:06 -0400

    feat: Add new character sprites and tilesets for DungeonEscape
    Introduce new male and female character sprites for clerics, heroes, soldiers, and wizards to enhance visual diversity in DungeonEscape. Accompanying tilesets for "hero male" and "soldier" roles are added to support the new sprite visuals.

---
+++ DungeonEscape.Unity/Assets/DungeonEscape/Tilesets/hero-m.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.1" name="hero male" tilewidth="32" tileheight="48" tilecount="20" columns="5">
+ <image source="../Images/sprites/hero-m.png" width="160" height="192"/>
+</tileset>
---
+++ DungeonEscape.Unity/Assets/DungeonEscape/Tilesets/soldier-m.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.1" name="soldier" tilewidth="32" tileheight="48" tilecount="20" columns="5">
+ <image source="../Images/sprites/soldier.png" width="160" height="192"/>
+</tileset>
